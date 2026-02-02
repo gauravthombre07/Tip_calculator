9c21e39532ab976df4e71562e70b8b0226ef6dcd
gauravthombre07 <uniquequotes369@gmail.com>
2026-02-02 18:48:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-let billAmountElement = (document.getElementById("billAmount"));
-let percentageTipElement = (document.getElementById("percentageTip"));
-let tipAmountElement = (document.getElementById("tipAmount"));
-let totalAmountElement = (document.getElementById("totalAmount"));
-
-let errorMessage = document.getElementById("errorMessage");
-
 function calculateButton() {
-    let billAmount = parseInt(billAmountElement.value);
-    let percentagetip = parseInt(percentageTipElement.value);
-    if (isNaN(billAmount) || isNaN(percentagetip)) {
-        errorMessage.textContent = "Please Enter a Valid Input";
+    let billAmount = parseFloat(document.getElementById("billAmount").value);
+    let percentageTip = parseFloat(document.getElementById("percentageTip").value);
+    let errorMessage = document.getElementById("errorMessage");
+
+    if (isNaN(billAmount) || isNaN(percentageTip)) {
+        errorMessage.textContent = "Please enter valid numbers";
         return;
     }
-    errorMessage.textContent = '';
-    let calculateTip = ((percentagetip / 100) * billAmount);
-    tipAmountElement.value = calculateTip.toFixed(2);
-    let total = billAmount + calculateTip;
-    totalAmountElement.value = total.toFixed(2);
+
+    errorMessage.textContent = "";
+
+    let tipAmount = (percentageTip / 100) * billAmount;
+    document.getElementById("tipAmount").value = tipAmount.toFixed(2);
+
+    let totalAmount = billAmount + tipAmount;
+    document.getElementById("totalAmount").value = totalAmount.toFixed(2);
 }
